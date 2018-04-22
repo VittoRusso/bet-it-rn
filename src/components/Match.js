bd45efb5b573b04c1eb031eeece37afb778b1b23
gaabgonca <32182942+gaabgonca@users.noreply.github.com>
2018-04-22 14:10:12 -0500

    Made 'Match.js' look a little better

--- src/components/Match.js
+++ src/components/Match.js
@@ -9,22 +9,24 @@ export default class Match extends React.PureComponent {
           <Text style={styles.date_text}>{this.props.date}</Text>
         </View>
         <View style={styles.row_one}>
-          <Text style={[styles.row_item, styles.left_align]}>
+          <View style={styles.img_ph} />
+          <Text style={[styles.row_item, styles.left_align]} numberOfLines={1}>
             {this.props.home_team}
             {""}
           </Text>
-          <Text style={[styles.row_item, styles.center_align]}>
+          <Text style={[styles.row_item_half, styles.center_align]}>
             {this.props.home_score ? this.props.home_score : "-"}
             {""}
           </Text>
-          <Text style={[styles.row_item, styles.center_align]}>
+          <Text style={[styles.row_item_half, styles.center_align]}>
             {this.props.away_score ? this.props.away_score : "-"}
             {""}
           </Text>
-          <Text style={[styles.row_item, styles.right_align]}>
+          <Text style={[styles.row_item, styles.right_align]} numberOfLines={1}>
             {this.props.away_team}
             {""}
           </Text>
+          <View style={styles.img_ph} />
         </View>
       </View>
     );
@@ -45,6 +47,10 @@ const styles = StyleSheet.create({
     flex: 1,
     fontSize: 16
   },
+  row_item_half: {
+    flex: 0.5,
+    fontSize: 16
+  },
   left_align: {
     textAlign: "left",
     fontWeight: "bold"
@@ -58,6 +64,14 @@ const styles = StyleSheet.create({
   },
   date_text: {
     fontSize: 14,
-    fontStyle: "italic"
+    fontStyle: "italic",
+    color: "#e74c3c",
+    opacity: 0.75
+  },
+  img_ph: {
+    backgroundColor: "#000000",
+    width: 40,
+    height: 40,
+    margin: 2
   }
 });

c4df35e4db4788af09e7efc6f29618cd44358590
VittoRusso <vitto1996@gmail.com>
2018-04-29 16:34:58 -0500

    bet json added to firebase

--- src/components/BetActivity.js
+++ src/components/BetActivity.js
@@ -9,10 +9,12 @@ import {
 } from "react-native";
 import { StackNavigator } from "react-navigation";
 import { Button } from "react-native-elements";
+import firebase from "react-native-firebase";
 
 export default class componentName extends Component {
   constructor(props) {
     super(props);
+    this.ref = firebase.firestore().collection("Bets");
     this.state = {
       match: this.props.navigation.state.params,
       home_score: 0,
@@ -59,7 +61,12 @@ export default class componentName extends Component {
   };
 
   _placeBet = () => {
-    Alert.alert("Bet");
+    this.state.match.home_score = this.state.home_score;
+    this.state.match.away_score = this.state.away_score;
+    this.ref.add(this.state.match).catch(err => {
+      console.log(err);
+    });
+    Alert.alert("Bet has been placed.");
   };
 
   _goBack = () => {
@@ -69,53 +76,64 @@ export default class componentName extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.row}>
-          <Text style={styles.title}> {this.state.match.home_team} </Text>
-          <Text style={styles.title}> {this.state.match.away_team} </Text>
+        <View style={styles.container}>
+          <View style={styles.row}>
+            <Text style={styles.title}> {this.state.match.home_team} </Text>
+            <Text style={styles.title}> {this.state.match.away_team} </Text>
+          </View>
+
+          <View style={styles.button_row}>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={this._increaseHomeScore}
+            >
+              <Text style={styles.button_text}>+</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={this._increaseAwayScore}
+            >
+              <Text style={styles.button_text}>+</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.row} flex={2}>
+            <Text style={styles.score}> {this.state.home_score} </Text>
+            <Text style={styles.score}> {this.state.away_score} </Text>
+          </View>
+
+          <View style={styles.button_row}>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={this._decreaseHomeScore}
+            >
+              <Text style={styles.button_text}>-</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={this._decreaseAwayScore}
+            >
+              <Text style={styles.button_text}>-</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-        <View style={styles.button_row}>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={this._increaseHomeScore}
-          >
-            <Text style={styles.button_text}>+</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={this._increaseAwayScore}
-          >
-            <Text style={styles.button_text}>+</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.row} flex={1}>
-          <Text style={styles.score}> {this.state.home_score} </Text>
-          <Text style={styles.score}> {this.state.away_score} </Text>
-        </View>
-        <View style={styles.button_row}>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={this._decreaseHomeScore}
-          >
-            <Text style={styles.button_text}>-</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={this._decreaseAwayScore}
-          >
-            <Text style={styles.button_text}>-</Text>
-          </TouchableOpacity>
-        </View>
-        <Text style={styles.title}> {this.state.match.date} </Text>
-        <Text style={styles.title}> {this.state.match.stadium} </Text>
-        <View style={styles.row}>
-          <TouchableOpacity style={styles.button} onPress={this._placeBet}>
-            <Text style={styles.button_text2}>PLACE BET</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.row}>
-          <TouchableOpacity style={styles.button} onPress={this._goBack}>
-            <Text style={styles.button_text2}>GO BACK</Text>
-          </TouchableOpacity>
+
+        <View style={styles.container}>
+          <Text style={styles.title}> {this.state.match.date} </Text>
+
+          <Text style={styles.title}> {this.state.match.stadium} </Text>
+
+          <View style={styles.row}>
+            <TouchableOpacity style={styles.button} onPress={this._placeBet}>
+              <Text style={styles.button_text2}>PLACE BET</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.row}>
+            <TouchableOpacity style={styles.button} onPress={this._goBack}>
+              <Text style={styles.button_text2}>GO BACK</Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
     );

d6228af98a50246bbd66a6a10dfffd351f1a6f8d
gaabgonca <32182942+gaabgonca@users.noreply.github.com>
2018-05-06 13:35:44 -0500

    Score system succesfully implemented on UserList.js

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, FlatList, Dimensions } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Dimensions,
+  RefreshControl
+} from "react-native";
 import firebase from "react-native-firebase";
 import { ListItem } from "react-native-elements";
 
@@ -12,25 +19,29 @@ export default class UserList extends React.PureComponent {
     this.state = {
       users: [],
       bets: [],
-      matches: []
+      matches: [],
+      refreshing: false
     };
   }
   componentDidMount() {
-    this.users_unsubscriber = this.users_ref.onSnapshot(this.onUsersUpdate);
+    this.bets_unsubscriber = this.bets_ref.onSnapshot(this.onBetsUpdate);
+
     this.matches_unsubscriber = this.matches_ref.onSnapshot(
       this.onMatchesUpdate
     );
-    this.bets_unsibscriber = this.bets_ref.onSnapshot(this.onBetsUpdate);
+
+    this.users_unsubscriber = this.users_ref.onSnapshot(this.onUsersUpdate);
   }
   componentWillUnmount() {
     this.users_unsubscriber();
     this.matches_unsubscriber();
+    this.bets_unsubscriber();
   }
   onUsersUpdate = querySnapshot => {
     var users = [];
     querySnapshot.forEach(doc => {
       var currentUser = doc.data();
-      currentUser.score = this.getScore(currentUser.email);
+      currentUser.score = this.getPoints(currentUser.email);
       users.push(currentUser);
     });
     this.setState({ users });
@@ -53,11 +64,80 @@ export default class UserList extends React.PureComponent {
       bets.push(doc.data());
     });
     this.setState({ bets });
+    var users = [];
+    this.users_ref.get().then(snapshot => {
+      this.onUsersUpdate(snapshot);
+    });
   };
 
-  getScore(email) {
-    return 0;
+  getPoints(email) {
+    var myBets = this.state.bets.filter(bet => bet.user == email);
+    var results = this.state.matches;
+    var score = 0;
+    for (var bet of myBets) {
+      var resultIndex = this.getResultIndex(bet.name, results);
+      if (resultIndex > -1) {
+        const result = results[resultIndex];
+        var increment = this.calcPoints(bet, result);
+        score = score + increment;
+      }
+    }
+    return score;
   }
+
+  calcPoints(bet, result) {
+    let bet_home = bet.home_result;
+    let bet_away = bet.away_result;
+    let result_home = result.home_result;
+    let result_away = result.away_result;
+    var points = 0;
+
+    //Home wins
+    if (result_home > result_away) {
+      //Bet on home winning
+      if (bet_home > bet_away) {
+        points = points + 1;
+        //Exact score predicted
+        if (bet_home == result_home && bet_away == result_away) {
+          points = points + 2;
+        }
+      }
+    }
+    //Away wins
+    else if (result_away > result_home) {
+      //Bet on away winning
+      if (bet_away > bet_home) {
+        points = points + 1;
+        //Exact  score predicted
+        if (bet_home == result_home && bet_away == result_away) {
+          points = points + 2;
+        }
+      }
+    }
+    //Tie wins
+    else {
+      //Bet on tie
+      if (bet_away == bet_home) {
+        points = points + 1;
+        if (bet_home == result_home && bet_away == result_away) {
+          points = points + 2;
+        }
+      }
+    }
+
+    return points;
+  }
+
+  getResultIndex(name, results) {
+    for (i = 0, len = results.length; i < len; ++i) {
+      var currentResult = results[i];
+      if (currentResult.name == name) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
   keyExtractor = (item, index) => index.toString();
 
   renderItem = ({ item }) => (
@@ -68,11 +148,29 @@ export default class UserList extends React.PureComponent {
     />
   );
 
+  reload() {
+    this.componentWillUnmount();
+    this.componentDidMount();
+  }
+
+  //This refreshes the score :D
+  _onRefresh() {
+    this.setState({ refreshing: true });
+    this.reload();
+    this.setState({ refreshing: false });
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.header}> Scoreboard </Text>
         <FlatList
+          refreshControl={
+            <RefreshControl
+              refreshing={this.state.refreshing}
+              onRefresh={this._onRefresh.bind(this)}
+            />
+          }
           keyExtractor={this.keyExtractor}
           data={this.state.users}
           renderItem={this.renderItem}
